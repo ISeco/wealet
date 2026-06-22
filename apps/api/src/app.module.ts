53208f0a3250184055a5176621280f57008e03b3
ISeco-agsbyte <ignacio.seco@agsbyte.cl>
2026-06-22 13:53:02 -0400

    setup database

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database')!,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
